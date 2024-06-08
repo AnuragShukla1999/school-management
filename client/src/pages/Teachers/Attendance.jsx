1ed99b7735e74456d0cada404123cf0ffe5457b1
AnuragShukla1999 <shuklaanurag310@gmail.com>
2024-06-08 16:11:09 +0530

    .

--- client/src/pages/Teachers/Attendance.jsx
+++ client/src/pages/Teachers/Attendance.jsx
@@ -63,8 +63,58 @@ const CheckAttendanceSection = () => {
     };
 
     return (
-        <></>
-    )
+        <AttendanceContainer>
+            <Sidebar/>
+
+            <Content>
+                <AttendanceContent>
+                    <AttendanceHeader>Attendance</AttendanceHeader>
+
+                    <AttendanceList>
+                        {students.map((student, index) => (
+                            <React.Fragment key={student.id} >
+                                <AttendanceItem>
+                                    <StudentName>{student.name}</StudentName>
+
+                                    <CheckboxLabel>
+                                        <input
+                                            type='checkbox'
+                                            checked={attendanceData[index]?.status === 'Present'}
+                                            onChange={() => handleStatusChange(student.id, 'Present')}
+                                        />
+                                        Present
+                                    </CheckboxLabel>
+
+
+
+                                    <CheckboxLabel>
+                                        <input
+                                            type='checkbox'
+                                            checked={attendanceData[index]?.status === 'Absent'}
+                                        />
+                                    </CheckboxLabel>
+
+
+
+                                    <CheckboxLabel>
+                                        <input
+                                            type='checkbox'
+                                            checked={attendanceData[index]?.status === 'Absent with apology'}
+                                        />
+                                        Absent with apology
+                                    </CheckboxLabel>
+                                </AttendanceItem>
+
+                                {index !== students.length - 1 && <Divider />} 
+                            </React.Fragment>
+                        ))}
+                    </AttendanceList>
+
+                    <SubmitButton onClick={handleSubmit} >Submit</SubmitButton>
+                </AttendanceContent>
+            </Content>
+        </AttendanceContainer>
+    );
 };
 
 export default CheckAttendanceSection;
